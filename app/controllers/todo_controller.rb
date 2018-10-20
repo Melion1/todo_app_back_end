0f6c44e5d4a764a6586eb60f5ffc597fdfd67e12
Melion Taliaferro <mdtaliaferro86@gmail.com>
2018-10-20 19:08:38 +0000

    Add conditional logic

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,25 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description = "Make the curriculum"
-        @todo_pomodoro_estimate = 4 
+        todo_id = params[:id]
+        
+        if todo_id == "1"
+          @todo_description = "Get rest at night"
+          @todo_pomodoro_estimate = 4 
+    
+       elsif todo_id == '2'
+        @todo_description = "Complete homework"
+        @todo_pomodoro_estimate = 3 
+        
+        elsif todo_id == '3'
+         @todo_description = "Be nice to Peers and staff"
+          @todo_pomodoro_estimate = 4 
+          
+         elsif todo_id == '4'
+           @todo_description = "Come to school on time"
+          @todo_pomodoro_estimate = 4 
+          
+          
+        end
     end 
 end
